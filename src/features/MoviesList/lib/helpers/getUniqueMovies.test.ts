20fec449af725cacb4d5c1a9d5142534e9417e62
anemone-swg <maks-11-01-2003@mail.ru>
2025-09-18 00:49:14 +0700

    test(unit): add tests for getUniqueMovies

--- src/features/MoviesList/lib/helpers/getUniqueMovies.test.ts
+++ src/features/MoviesList/lib/helpers/getUniqueMovies.test.ts
@@ -2,7 +2,7 @@ import { getUniqueMovies } from "./getUniqueMovies";
 import { Movie } from "../../model/types/movie";
 
 describe("getUniqueMovies", () => {
-  const mockMovies: Movie[] = [
+  const mockMovies1: Movie[] = [
     {
       imdbID: "1",
       Title: "Movie 1",
@@ -19,6 +19,47 @@ describe("getUniqueMovies", () => {
     },
     {
       imdbID: "2",
+      Title: "Movie 2",
+      Year: "2021",
+      Type: "series",
+      Poster: "N/A",
+    },
+  ];
+
+  const mockMovies2: Movie[] = [
+    {
+      imdbID: "1",
+      Title: "Movie 1",
+      Year: "2020",
+      Type: "movie",
+      Poster: "N/A",
+    },
+    {
+      imdbID: "2",
+      Title: "Movie 2",
+      Year: "2021",
+      Type: "series",
+      Poster: "N/A",
+    },
+  ];
+
+  const mockMovies3: Movie[] = [
+    {
+      imdbID: "1",
+      Title: "Movie 1",
+      Year: "2020",
+      Type: "movie",
+      Poster: "N/A",
+    },
+    {
+      imdbID: "1",
+      Title: "Movie 1 duplicate",
+      Year: "2020",
+      Type: "movie",
+      Poster: "N/A",
+    },
+    {
+      imdbID: "1",
       Title: "Movie 2",
       Year: "2021",
       Type: "series",
@@ -43,6 +84,12 @@ describe("getUniqueMovies", () => {
     //     Poster: "N/A",
     //   },
     // ]);
-    expect(getUniqueMovies(mockMovies)).toMatchSnapshot();
+    expect(getUniqueMovies(mockMovies1)).toMatchSnapshot();
+  });
+  test("нет дубликатов", () => {
+    expect(getUniqueMovies(mockMovies2)).toEqual(mockMovies2);
+  });
+  test("все дубликаты", () => {
+    expect(getUniqueMovies(mockMovies3)).toEqual([mockMovies3[0]]);
   });
 });
