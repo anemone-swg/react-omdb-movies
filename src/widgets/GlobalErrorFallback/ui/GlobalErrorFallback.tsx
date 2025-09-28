cfb35ac8e202a3864732c9cb7112b0b3ebdb5f3f
anemone-swg <maks-11-01-2003@mail.ru>
2025-09-28 23:36:31 +0700

    chore: fix styles and add i18n for GlobalErrorFallback

--- src/widgets/GlobalErrorFallback/ui/GlobalErrorFallback.tsx
+++ src/widgets/GlobalErrorFallback/ui/GlobalErrorFallback.tsx
@@ -5,10 +5,17 @@ import { FallbackProps } from "react-error-boundary";
 const GlobalErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   const { t } = useTranslation();
   return (
-    <div>
+    <div className="flex flex-col items-center gap-2">
       <p>{t("возникла_непредвиденная_ошибка")}</p>
       <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again.</button>
+      <button
+        className={
+          "bg-button hover:bg-button-hover transition-colors text-white px-4 py-2 rounded"
+        }
+        onClick={resetErrorBoundary}
+      >
+        {t("попробовать_снова")}
+      </button>
     </div>
   );
 };
